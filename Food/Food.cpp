68d709c621905e416bf0e62a8d6cfc0687fae027
CH <ch@CHs-MacBook-Pro.local>
2022-10-02 23:09:00 +0200

    Debug with memory leak.

--- Food/Food.cpp
+++ Food/Food.cpp
@@ -1,1 +1,11 @@
 #include "Food.hpp"
+#include "../List/List.hpp"
+
+template <>
+void List<Food>::Remove(ListCursor<Food> *cursor)
+{
+    item *tmp = (*cursor->itm)->next;
+    delete (*cursor->itm)->val;
+    delete (*cursor->itm);
+    (*cursor->itm) = tmp;
+}
